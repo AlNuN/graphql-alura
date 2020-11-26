f95dc23757ab93956549af6978c3a34822c55f6a
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-11-26 16:10:48 -0300

    Minimal working graphql server

---
+++ api/index.js
@@ -0,0 +1,13 @@
+const { ApolloServer } = require('apollo-server');
+const { mergeTypeDefs } = require('graphql-tools');
+const userSchema = require('./user/schema/user.graphql');
+const userResolvers = require('./user/resolvers/userResolvers');
+
+const typeDefs = mergeTypeDefs([userSchema]);
+const resolvers = [userResolvers];
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({url}) => {
+  console.log(`Servidor rodando no endereço ${url}`);
+});
