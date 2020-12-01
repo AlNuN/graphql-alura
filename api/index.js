765abd3b04e7904d56e0e40ea949086a0d729252
Matheus Alves Nunes <matheus.alnun@gmail.com>
2020-12-01 13:21:13 -0300

    Add apollo datasource rest and use it to consume REST API

--- api/index.js
+++ api/index.js
@@ -2,11 +2,18 @@ const { ApolloServer } = require('apollo-server');
 const { mergeTypeDefs } = require('graphql-tools');
 const userSchema = require('./user/schema/user.graphql');
 const userResolvers = require('./user/resolvers/userResolvers');
+const UsersAPI = require('./user/datasource/user');
 
 const typeDefs = mergeTypeDefs([userSchema]);
 const resolvers = [userResolvers];
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const server = new ApolloServer({ 
+  typeDefs, 
+  resolvers,
+  dataSources: () => ({
+    usersAPI: new UsersAPI(),
+  }),
+});
 
 server.listen().then(({url}) => {
   console.log(`Servidor rodando no endereço ${url}`);
